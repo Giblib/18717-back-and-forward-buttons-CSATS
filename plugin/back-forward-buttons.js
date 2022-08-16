fc291ceb94621b9fcca8d6978941a383575ae08a
Henry <hpduong@users.noreply.github.com>
2022-08-16 12:01:58 -0700

    update forward backward skip images to 30s

--- plugin/back-forward-buttons.js
+++ plugin/back-forward-buttons.js
@@ -13,9 +13,9 @@ videojs.registerPlugin('backForwardButtons', function() {
   newElementFB.id = 'forwardButton';
 
   // +++ Assign properties to elements and assign to parents +++
-  newImageBB.setAttribute('src', 'https://s3-us-west-1.amazonaws.com/v4.0-assets/backwards.svg');
+  newImageBB.setAttribute('src', 'https://raw.githubusercontent.com/Giblib/18717-back-and-forward-buttons-CSATS/master/img/Backward_30-01.svg');
   newElementBB.appendChild(newImageBB);
-  newImageFB.setAttribute('src', 'https://raw.githubusercontent.com/Giblib/18717-back-and-forward-buttons-CSATS/master/img/Forward_30s.svg');
+  newImageFB.setAttribute('src', 'https://https://raw.githubusercontent.com/Giblib/18717-back-and-forward-buttons-CSATS/master/img/Forward_30-01.svg');
   newElementFB.appendChild(newImageFB);
 
   // +++ Get controlbar and insert elements +++
